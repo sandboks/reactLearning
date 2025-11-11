fab8610d0ba8b47f11374cb7889186e20d878d90
Sand <sandboxasdf@gmail.com>
2025-11-11 22:27:32 +1100

    implemented the entry function with hard coded data

--- react2travel/src/components/Entry.jsx
+++ react2travel/src/components/Entry.jsx
@@ -14,7 +14,8 @@
  *   this data into the component.
  */
 
-import globeLogo from '../assets/logo.png'
+import globeLogo from '../assets/logo.png';
+import imgLocationMarker from '../assets/marker.png';
 let imgurl = "https://scrimba.com/links/travel-journal-japan-image-url";
 
 export default function Entry() {
@@ -26,12 +27,16 @@ export default function Entry() {
                         <img src={imgurl}/>
                     </div>
                     <div class="EntryRightColumn">
-                        <div>
-                            <span>Japan view on Google maps</span>
+                        <div class="countryRow">
+                            <div class="countryRow countryLocation">
+                                <img src={imgLocationMarker}></img>
+                                <span>Japan</span>
+                            </div>
+                            <span><a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google maps</a></span>
                         </div>
                         <h1>Mount Fuji</h1>
-                        <span>12 Jan, 2021 - 24 Jan, 2021</span>
-                        <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                        <span class="dateSpan">12 Jan, 2021 - 24 Jan, 2021</span>
+                        <p class="entryBlurb">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
                     </div>
                 </div>
             </article>
