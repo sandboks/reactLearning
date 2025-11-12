d54834fadd11b33994d9a4b4ffccce568dd8b751
Sand <sandboxasdf@gmail.com>
2025-11-12 13:09:20 +1100

    using react props to fill in all the data for one entry

--- react2travel/src/components/Entry.jsx
+++ react2travel/src/components/Entry.jsx
@@ -18,25 +18,28 @@ import globeLogo from '../assets/logo.png';
 import imgLocationMarker from '../assets/marker.png';
 let imgurl = "https://scrimba.com/links/travel-journal-japan-image-url";
 
-export default function Entry() {
+export default function Entry(props) {
     return (
         <>
             <article class="EntryRoot">
                 <div class="EntryContentGrid">
                     <div class="EntryLeftImageContainer">
-                        <img src={imgurl}/>
+                        <img 
+                            src={props.img.src} 
+                            alt={props.img.alt}
+                        />
                     </div>
                     <div class="EntryRightColumn">
                         <div class="countryRow">
                             <div class="countryRow countryLocation">
                                 <img src={imgLocationMarker}></img>
-                                <span>Japan</span>
+                                <span>{props.country}</span>
                             </div>
-                            <span><a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google maps</a></span>
+                            <span><a href={props.mapsUrl}>View on Google maps</a></span>
                         </div>
                         <h1>Mount Fuji</h1>
-                        <span class="dateSpan">12 Jan, 2021 - 24 Jan, 2021</span>
-                        <p class="entryBlurb">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                        <span class="dateSpan">{props.dates}</span>
+                        <p class="entryBlurb">{props.blurb}</p>
                     </div>
                 </div>
             </article>
